942b159a1dece5773433662cef657a704915f625
Sarushan <sarushan1994@gmail.com>
2024-01-24 21:45:55 +0530

    linkedIn image

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-gray-800 text-white p-4">
       <div className="flex items-center justify-center">
         <img
-          src="/sharushan.jpg"
+          src="https://media.licdn.com/dms/image/D5603AQFat05aTRvjIw/profile-displayphoto-shrink_400_400/0/1692079695101?e=1711584000&v=beta&t=8ZccjGCl6g5ACj4DpsfglIT4dAgdlMlzfsgZ6P1yy6s"
           alt="sharushan"
           className="w-20 h-20 rounded-full mr-4 border-2 border-white"
         />
